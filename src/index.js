e5d9f88aef78382ee7628ccecdf126b7d00f1ce1
juli_ko <yuliakolomiets09@gmail.com>
2023-02-15 00:46:16 +0300

    feat:brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = 0;
+  for (let i = 0; i <= str.length; i++) {
+    if (str[i] === "[" || str[i] === "{" || str[i] === "(") flag += 1;
+    if (str[i] === "]" || str[i] === "}" || str[i] === ")") flag -= 1;
+    if (flag < 0) return false;
+    if (str[i] === "[" && (str[i + 1] === ")" || str[i + 1] === "}"))
+      return false;
+    if (str[i] === "(" && (str[i + 1] === "]" || str[i + 1] === "}"))
+      return false;
+    if (str[i] === "{" && (str[i + 1] === "]" || str[i + 1] === ")"))
+      return false;
+    if (str[i] === "1" && (str[i + 1] === "4" || str[i + 1] === "6"))
+      return false;  
+  }
+  if (flag === 0) {
+    str.split("|").forEach((element) => {
+      if (element.length % 2 === 1) flag -= 1;
+    });
+  }
+
+  if (flag === 0) {
+    if (str.split("|").length % 2 === 1) return true;
+  }
+
+  return false;
 }
